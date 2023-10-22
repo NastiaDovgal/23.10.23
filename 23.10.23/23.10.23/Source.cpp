73a4e3012ffa0772932c40d69ff1e8d88d97b7d5
Настя <nastadovgal005@gmail.com>
2023-10-23 00:39:57 +0300

    Edit function main

--- 23.10.23/23.10.23/Source.cpp
+++ 23.10.23/23.10.23/Source.cpp
@@ -13,6 +13,24 @@ int main()
 	file1 = fopen("C:\\Users\\admin\\Desktop\\12345.txt", "rb");
 	file2 = fopen("C:\\Users\\admin\\Desktop\\123.txt", "wb");
 
-	
+	if (file1 != nullptr && file2 != nullptr)
+	{
+		char str[500];
+		int countLine = 0;
+		while (fgets(str, 500, file1))
+			countLine++;
+
+		int lastLine = 0;
+		int count = 0;
+		int x = 0;
+		bool y = 0;
+		
+
+		fclose(file1);
+		fclose(file2);
+	}
+	else
+		cout << "Помилка відкриття файлів" << endl;
+
 	return 0;
 }
